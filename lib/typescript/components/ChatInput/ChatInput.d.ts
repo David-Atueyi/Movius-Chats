b9fae2fac3208d0ee11d84ba017f8dad3a19b336
David-Atueyi <atueyidavid2005@gmail.com>
2026-07-11 01:04:58 +0100

    fixed the media viewer swipe issue updated the readme

--- lib/typescript/components/ChatInput/ChatInput.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { ChatInputProps } from './types';
-declare const _default: React.NamedExoticComponent<ChatInputProps>;
-export default _default;
--- lib/typescript/hooks/useKeyboardInset.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function useKeyboardInset(enabled?: boolean): number;
